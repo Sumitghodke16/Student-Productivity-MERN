6143f523992e399389791dbc86de2a2ff3ea5745
Sumitghodke16 <sumitghodke116@gmail.com>
2026-05-27 13:57:35 +0530

    updated deployment configuration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-productivity-mern.onrender.com/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 // ADD TOKEN AUTOMATICALLY
